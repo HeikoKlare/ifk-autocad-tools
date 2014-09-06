0ee05450c2f6819a72061602ce0d236792dd101d
Heiko <klare.heiko@gmail.com>
2014-09-07 00:04:41 +0200

    consecutive dimension works again, syncronous executing is implemented, reference point can be used without having an object at that point now, dimension is non associative now

--- AutoCAD Tools/Tools/ConsecutiveDimension.cs
+++ AutoCAD Tools/Tools/ConsecutiveDimension.cs
@@ -6,6 +6,7 @@ using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.EditorInput;
 using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
 
 namespace AutoCADTools.Tools
 {
@@ -19,7 +20,7 @@ namespace AutoCADTools.Tools
             Document acDoc = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
             Dictionary<LayerTableRecord, bool> layerActivation = new Dictionary<LayerTableRecord, bool>();
 
-            using (Transaction acTrans = acDoc.TransactionManager.StartTransaction())
+            using (Transaction acTrans = acDoc.Database.TransactionManager.StartTransaction())
             {
                 // Save all layers status and turn most of them off
                 LayerTable layerTable = acTrans.GetObject(acDoc.Database.LayerTableId, OpenMode.ForWrite) as LayerTable;
@@ -40,7 +41,7 @@ namespace AutoCADTools.Tools
                         current.IsOff = False;
                     }
                 }
-                
+
                 // Let the user select the objects
                 var selection = acDoc.Editor.GetSelection();
                 if (selection.Status != PromptStatus.OK)
@@ -49,12 +50,13 @@ namespace AutoCADTools.Tools
                     return;
                 }
                 // Create a handle string for the objects
-                string handleString = "";
+                //string handleString = "";
                 Point3d minimum = new Point3d(double.MaxValue, double.MaxValue, 0);
                 Point3d maximum = new Point3d(double.MinValue, double.MinValue, 0);
+                
                 foreach (SelectedObject obj in selection.Value)
                 {
-                    handleString += "(handent \"" + obj.ObjectId.Handle + "\")" + "\r";
+                    //handleString += "(handent \"" + obj.ObjectId.Handle + "\")" + "\r";
                     try
                     {
                         Line l = acTrans.GetObject(obj.ObjectId, OpenMode.ForRead) as Line;
@@ -67,7 +69,7 @@ namespace AutoCADTools.Tools
                         if (l.EndPoint.Y < minimum.Y) minimum = new Point3d(minimum.X, l.EndPoint.Y, 0);
                         if (l.EndPoint.Y > maximum.Y) maximum = new Point3d(maximum.X, l.EndPoint.Y, 0);
                     }
-                    catch (Exception) { };
+                    catch (System.Exception) { };
                 }
                 Point3d average = new Point3d((maximum.X + minimum.X) / 2, (maximum.Y + minimum.Y) / 2, 0);
 
@@ -93,14 +95,14 @@ namespace AutoCADTools.Tools
                 PromptPointOptions pointOpts = new PromptPointOptions(Environment.NewLine + LocalData.DimensionInsertionPoint);
                 pointOpts.UseBasePoint = true;
                 pointOpts.BasePoint = referencePoint.Value;
-                var insertionPoint = acDoc.Editor.GetPoint(pointOpts);
+                PromptPointResult insertionPoint = acDoc.Editor.GetPoint(pointOpts);
                 Autodesk.AutoCAD.ApplicationServices.Application.SetSystemVariable("AUTOSNAP", oldAutoSnap);
                 if (insertionPoint.Status != PromptStatus.OK)
                 {
                     acTrans.Abort();
                     return;
                 }
-                var insertionPointString = insertionPoint.Value.X + "," + insertionPoint.Value.Y + "," + insertionPoint.Value.Z;
+                //var insertionPointString = insertionPoint.Value.X + "," + insertionPoint.Value.Y + "," + insertionPoint.Value.Z;
 
                 // Let the user define the number of decimal places
                 PromptIntegerOptions intOpts = new PromptIntegerOptions(Environment.NewLine + LocalData.DimensionDecimalPlaces);
@@ -108,7 +110,7 @@ namespace AutoCADTools.Tools
                 intOpts.LowerLimit = 1;
                 intOpts.UpperLimit = 4;
                 intOpts.UseDefaultValue = true;
-                var decimalPlaces = acDoc.Editor.GetInteger(intOpts);
+                PromptIntegerResult decimalPlaces = acDoc.Editor.GetInteger(intOpts);
                 if (decimalPlaces.Status != PromptStatus.OK) 
                 {
                     acTrans.Abort();
@@ -129,7 +131,7 @@ namespace AutoCADTools.Tools
                     refPoint = refPoint.Add(new Vector3d(0, referencePoint.Value.Y < average.Y ? -0.001 : 0.001, 0));
                     calcRefPoint = new Point3d(referencePoint.Value.X < average.X ? minimum.X : maximum.X, referencePoint.Value.Y, 0);
                 }
-                var referencePointString = refPoint.X + "," + refPoint.Y + "," + refPoint.Z;
+                //var referencePointString = refPoint.X + "," + refPoint.Y + "," + refPoint.Z;
 
                 // Restore old ortho mode
                 acDoc.Database.Orthomode = oldOrtho;
@@ -144,11 +146,12 @@ namespace AutoCADTools.Tools
                 var oldDimexo = acDoc.Database.Dimexo;
                 var oldDimdec = acDoc.Database.Dimdec;
                 var oldDimdli = acDoc.Database.Dimdli;
-
+                var oldDimAssoc = acDoc.Database.DimAssoc;
+                
                 if (dimStyleTable.Has(STYLENAME))
                 {
                     acDoc.Database.Dimstyle = dimStyleTable[STYLENAME];
-                    var rec = acTrans.GetObject(dimStyleTable[STYLENAME], OpenMode.ForRead) as DimStyleTableRecord;
+                    var rec = acTrans.GetObject(dimStyleTable[STYLENAME], OpenMode.ForWrite) as DimStyleTableRecord;
                     acDoc.Database.SetDimstyleData(rec);
                     acDoc.Database.Dimexo = dist * double.Parse(Autodesk.AutoCAD.ApplicationServices.Application.GetSystemVariable("CANNOSCALEVALUE").ToString()) - 0.002;
                 }
@@ -161,10 +164,14 @@ namespace AutoCADTools.Tools
                 }
                 acDoc.Database.Dimdec = decimalPlaces.Value;
                 acDoc.Database.Dimdli = 0.0;
-                                               
+                // Make non associative dimension
+                acDoc.Database.DimAssoc = 1;
+
                 // Execute command
-                acDoc.SendStringToExecute("SBEM " + handleString + " BA P " + referencePointString + "\r" + insertionPointString + " ", true, false, true);
                 acTrans.Commit();
+                acDoc.Editor.Command("SBEM", selection.Value, "", "BE", "H", refPoint, "X", "BA", "P", refPoint, insertionPoint.Value);
+                
+                //acDoc.SendStringToExecute("SBEM " + handleString + " BA P " + referencePointString + "\r" + insertionPointString + " ", true, false, true);
 
                 // Restore old values
                 acDoc.Database.Dimstyle = oldDimStyle;
@@ -172,6 +179,8 @@ namespace AutoCADTools.Tools
                 acDoc.Database.Dimexo = oldDimexo;
                 acDoc.Database.Dimdec = oldDimdec;
                 acDoc.Database.Dimdli = oldDimdli;
+                acDoc.Database.DimAssoc = oldDimAssoc;
+
             }
         }
     }
