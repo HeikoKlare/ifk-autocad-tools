c6591cccf2c61ab5bb49c6a180bbe9770a102eaf
Heiko Klare <klare.heiko@gmail.com>
2020-01-09 12:30:02 +0100

    Adapted settings to database URL change

--- AutoCAD Tools/Properties/Settings.Designer.cs
+++ AutoCAD Tools/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace AutoCADTools.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.1.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.4.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -145,7 +145,7 @@ namespace AutoCADTools.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("ifk.diskstation.me")]
+        [global::System.Configuration.DefaultSettingValueAttribute("SERVER_URL")]
         public string SqlConnectionPath {
             get {
                 return ((string)(this["SqlConnectionPath"]));
